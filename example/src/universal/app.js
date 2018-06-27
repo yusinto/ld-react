a3d40e8aee308c06ca082a1e024b33057cd1d2ea
Yusinto Ngadiman <yusinto@gmail.com>
2018-06-27 14:42:51 +1000

    Improved grid layout. Added animation back and forth. Added arrow.

--- example/src/universal/app.js
+++ example/src/universal/app.js
@@ -12,17 +12,6 @@ const GridContainer = styled.div`
   grid-template-rows: 30px 50px;
 `;
 
-const GridItem = styled.div`
-  background: #6772e5;
-  grid-column: 3 / span 3
-  display: flex;
-  flex-direction: row;
-  justify-content: space-around;
-  align-items: center;
-  color: #fff;
-  position: relative;
-`;
-
 const ArrowUp = styled.div`
   margin-left: 40px;
   width: 0; 
@@ -31,48 +20,6 @@ const ArrowUp = styled.div`
   border-right: 5px solid transparent;
   border-bottom: 5px solid #73AD21;
 `;
-
-const MenuTitle = styled.div`
-  &:hover {
-    opacity: 0.5;
-  }
-  flex-grow: 1;
-  text-align: center;
-`;
-
-const MoveItemDevelopers = keyframes`
-  from {
-    left: 180px;
-  }
-  
-  to {
-    left: 280px; 
-  }
-`;
-
-const MoveItemCompany = keyframes`
-  from {
-    left: 280px;
-  }
-  
-  to {
-    left: 380px; 
-  }
-`;
-
-const GridItem2 = styled.div`
-  // background: #73AD21;
-  grid-column: 1 / span 6
-  grid-row: 2 / span 1
-  display: flex;
-  flex-direction: row;
-  justify-content: space-around;
-  align-items: center;
-  color: #fff;
-  position: relative;
-  align-self: start;
-`;
-
 const ShowItem = keyframes`
   from {
     opacity: 0;
@@ -94,94 +41,66 @@ const HideItem = keyframes`
   }
 `;
 
-const ProductItem = styled.div`
+const GridItem = styled.div`
+  background: #6772e5;
+  grid-column: 3 / span 3;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
+  color: #fff;
+  position: relative;
+`;
+const MenuTitle = styled.div`
+  &:hover {
+    opacity: 0.5;
+  }
+  flex-grow: 1;
+  text-align: center;
+`;
+
+const Move = (moveFrom, moveTo) => keyframes`
+  from {
+    left: ${moveFrom};
+  }
+  
+  to {
+    left: ${moveTo};
+  }
+`;
+
+const MovingDiv = styled.div`
+  width: 100px;
+  height: 100px;
+  position: absolute;
+  top: 30px;
+  left: -10px;
+  animation: ${({moveFrom, moveTo}) => Move(moveFrom, moveTo)} 0.3s forwards ease;
+  //animation-duration: 0.2s;
+  //animation-fill-mode: forwards;
+`;
+
+const MovingDivContent = styled.div`
   background: #73AD21;
-  position: absolute;
-  top: 0;
-  left: 180px;
-  width: 250px;
-  height: 350px;
-  opacity: 1;
   border-radius: 5px;
-  animation-duration: ${props => {
-  if (props.animation === 'move') return '0.4s';
-  if (props.animation === 'show') return '0.6s';
-  return '0.8s';
-}
-  };
-  animation-name: ${({animation, category}) => {
-  if (animation === 'move') {
-    if (category === 'products') return ShowItem;
-    if (category === 'developers') return MoveItemDevelopers;
-    if (category === 'company') return MoveItemCompany;
-  }
-
-  if (animation === '') {
-    return '';
-  }
-
-  return animation === 'show' ? ShowItem : HideItem;
-}};
-  animation-fill-mode: forwards;
+  width: 100%;
 `;
 
-const Item = styled.div`
-  background: ${props => {
-  switch(props.parent) {
-    case 'developers':
-      return 'red';
-    case 'company':
-      return 'blue';
-    default:
-      return '#73AD21';
-  }
-}};
-  position: absolute;
-  top: 0;
-  left: ${props => {
-    switch(props.parent) {
-      case 'developers':
-        return '280px';
-      case 'company':
-        return '380px';
-      default:
-        return '180px';
-    }
-}};
-  width: 250px;
-  height: 350px;
-  opacity: 1;
-  border-radius: 5px;
-  opacity: 0;
-  animation-duration: 0.8s;
-  animation-name: ${props => {
-    
-}}
-`;
+const getX = {products: '-10px', developers: '90px', company: '190px'};
 
 class App extends Component {
-  state = {animation: ''};
+  state = {moveFrom: '-10px', moveTo: '-10px'};
 
-  onMouseEnter = () => {
+  onMouseEnter = (category) => {
     this.setState((prevState) => {
-      if (!prevState.animation || prevState.animation === 'hide') { // initial state
-        return {animation: 'show'};
-      }
+      const moveFrom = prevState.moveTo;
+      const moveTo = getX[category];
+
+      return {
+        moveFrom,
+        moveTo,
+      };
     });
-  };
-
-  onMouseLeave = () => {
-    this.setState({animation: 'hide'});
-  };
-
-  onMouseEnterTitle = (category) => {
-    console.log('enterTitle');
-    // this.setState({animation: 'move', category});
-  };
-
-  onMouseLeaveTitle = () => {
-    console.log('leaveTitle');
-    // this.setState({animation: 'hide'});
   };
 
   render() {
@@ -190,31 +109,19 @@ class App extends Component {
         <header>
           <nav>
             <GridContainer>
-              <GridItem onMouseEnter={this.onMouseEnter} onMouseLeave={this.onMouseLeave}>
-                <MenuTitle onMouseEnter={() => this.onMouseEnterTitle('products')}
-                           onMouseLeave={this.onMouseLeaveTitle}>
-                  Products
-                </MenuTitle>
-                <MenuTitle onMouseEnter={() => this.onMouseEnterTitle('developers')}
-                           onMouseLeave={this.onMouseLeaveTitle}>Developers</MenuTitle>
-                <MenuTitle onMouseEnter={() => this.onMouseEnterTitle('company')} onMouseLeave={this.onMouseLeaveTitle}>Company</MenuTitle>
+              <GridItem>
+                <MenuTitle onMouseEnter={() => this.onMouseEnter('products')}>Products</MenuTitle>
+                <MenuTitle onMouseEnter={() => this.onMouseEnter('developers')}>Developers</MenuTitle>
+                <MenuTitle onMouseEnter={() => this.onMouseEnter('company')}>Company</MenuTitle>
+                <MovingDiv moveFrom={this.state.moveFrom} moveTo={this.state.moveTo}>
+                  <ArrowUp/>
+                  <MovingDivContent>
+                    <div>item 1</div>
+                    <div>item 2</div>
+                    <div>item 3</div>
+                  </MovingDivContent>
+                </MovingDiv>
               </GridItem>
-              <GridItem2>
-                <Item parent="products">
-                  <div>Payments</div>
-                  <div>Billing</div>
-                  <div>Connect</div>
-                </Item>
-                <Item parent="developers">
-                  <div>Documentation</div>
-                  <div>Api</div>
-                </Item>
-                <Item parent="company">
-                  <div>About us</div>
-                  <div>Customers</div>
-                  <div>Jobs</div>
-                </Item>
-              </GridItem2>
             </GridContainer>
           </nav>
         </header>
