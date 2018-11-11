f2aca6a9e1ffc62207367ae42c3901c38999d607
Yusinto Ngadiman <yus@launchdarkly.com>
2018-11-12 08:27:26 +1100

    Better tests for useLaunchDarkly hook.

--- src/useLaunchDarkly.test.js
+++ src/useLaunchDarkly.test.js
@@ -3,6 +3,7 @@ jest.mock('react', () => {
   return {
     ...original,
     useState: jest.fn(),
+    useEffect: jest.fn(),
   }
 });
 jest.mock('./initLDClient', () => ({
@@ -12,63 +13,64 @@ jest.mock('./initLDClient', () => ({
   },
 }));
 
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import TestRenderer from 'react-test-renderer';
 
 import {initLDClient, ldClient} from './initLDClient';
 import useLaunchDarkly from './useLaunchDarkly';
 
 const clientSideId = 'deadbeef';
-const flags = {'testFlag': true, 'anotherTestFlag': true};
+const mockFlags = {'testFlag': true, 'anotherTestFlag': true};
 
 describe('useLaunchDarkly hook', () => {
-  const mockSetFlags = jest.fn();
+  let mockSetFlags = jest.fn();
 
   beforeEach(() => {
-    initLDClient.mockImplementation(() => flags);
-    useState.mockImplementation(() => [flags, mockSetFlags]);
+    initLDClient.mockImplementation(() => mockFlags);
+    useState.mockImplementation(() => [{}, mockSetFlags]);
   });
 
   afterEach(() => {
     jest.resetAllMocks();
   });
 
-  test('initialise ld client and subscribe on mount', () => {
+  test('render app', () => {
     const App = () => <div>My App</div>;
     const LaunchDarklyApp = () => useLaunchDarkly(App, ({clientSideId}));
     const component = TestRenderer.create(<LaunchDarklyApp />);
 
-    // GOTCHA: we need to re-render a second time to trigger useEffect hooks
-    // https://github.com/kentcdodds/react-testing-library/issues/215
-    component.update(<LaunchDarklyApp />);
-
-    expect(initLDClient.mock.calls[0][0]).toBe(clientSideId);
-    ldClient.on.mockImplementation((e, cb) => {
-      expect(e).toEqual('change');
-      cb();
-    });
     expect(component).toMatchSnapshot();
   });
 
-  test('setFlags is called in both useEffect and subscribe', () => {
+  test('setFlags is called in useEffect', async () => {
     const App = () => <div>My App</div>;
     const LaunchDarklyApp = () => useLaunchDarkly(App, ({clientSideId}));
-    const component = TestRenderer.create(<LaunchDarklyApp />);
-    component.update(<LaunchDarklyApp />);
+    TestRenderer.create(<LaunchDarklyApp />);
 
-    // first useEffect, then in subscribe
-    mockSetFlags
-      .mockImplementationOnce(remoteFlags => {
-        expect(remoteFlags).toEqual(flags);
-      })
-      .mockImplementationOnce(remoteFlags => {
-        expect(remoteFlags).toEqual({"anotherTestFlag": true, "testFlag": false});
-      });
+    const asyncEffect = useEffect.mock.calls[0][0];
+    const props = useEffect.mock.calls[0][1];
+    await asyncEffect();
 
-    ldClient.on.mockImplementation((e, cb) => {
-      expect(e).toEqual('change');
-      cb({'test-flag': {current: false, previous: true}});
-      expect(mockSetFlags.mock.calls.length).toBe(2);
-    });
+    expect(useEffect).toHaveBeenCalledTimes(1);
+    expect(useEffect).toHaveBeenCalledWith(asyncEffect, props);
+    expect(initLDClient).toHaveBeenCalledWith(clientSideId, undefined, undefined);
+    expect(mockSetFlags).toHaveBeenCalledWith(mockFlags);
+  });
+
+  test('subscribe to changes', async () => {
+    const mockSetFlags = jest.fn();
+    useState.mockImplementation(() => [mockFlags, mockSetFlags]);
+    ldClient.on.mockImplementation((e, cb) => cb({'test-flag': {current: false, previous: true}}));
+
+    const App = () => <div>My App</div>;
+    const LaunchDarklyApp = () => useLaunchDarkly(App, ({clientSideId}));
+    TestRenderer.create(<LaunchDarklyApp />);
+
+    const asyncEffect = useEffect.mock.calls[0][0];
+    await asyncEffect();
+
+    expect(mockSetFlags).toHaveBeenCalledTimes(2);
+    expect(mockSetFlags).toHaveBeenCalledWith(mockFlags);
+    expect(mockSetFlags).toHaveBeenLastCalledWith({'testFlag': false, 'anotherTestFlag': true});
   });
 });
