9b84ea107c06e71a652594f2770f92dda00aa0aa
Yusinto Ngadiman <yus@launchdarkly.com>
2018-11-12 08:48:04 +1100

    Added hoc example.

---
+++ examples/hoc/src/universal/app.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import {Switch, Link, Route, Redirect} from 'react-router-dom';
+import Home from './home';
+import Contact from './contact';
+import {withFlagProvider} from 'ld-react';
+
+const App = () =>
+  <div>
+    <header>
+      <nav>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/contact">Contact</Link></li>
+        </ul>
+      </nav>
+    </header>
+    <main>
+      <Switch>
+        <Route exact path="/" component={Home}/>
+        <Route path="/home">
+          <Redirect to="/"/>
+        </Route>
+        <Route path="/contact" component={Contact}/>
+      </Switch>
+    </main>
+  </div>;
+
+// Set clientSideId to your own Client-side ID. You can find this in
+// the dashboard under Account settings / Projects
+export default withFlagProvider(App, {clientSideId: '59b2b2596d1a250b1c78baa3'});
---
+++ examples/hoc/src/universal/home.js
@@ -0,0 +1,45 @@
+import React, {Component} from 'react';
+import {withFlags} from 'ld-react';
+
+class Home extends Component {
+  state = {randomNumber: 0};
+
+  onClickGenerateRandom = () => {
+    const min = 1;
+    const max = 100;
+    const randomNumber = Math.floor(Math.random() * (max - min)) + min;
+    this.setState({randomNumber});
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Welcome to ld-react example</h1>
+        <div>
+          To run this example:
+          <ul>
+            <li>In app.js, set clientSideId to your own Client-side ID. You can find this in the dashboard underAccount settings / Projects.</li>
+            <li>Create a flag called 'dev-test-flag' in your project and turn it on and off.</li>
+          </ul>
+        </div>
+        {
+          this.props.flags.devTestFlag ?
+            <div>
+              <p>
+                SSE works! If you turn off your flag in launch darkly, your app will respond without a browser refresh.
+                Try it!
+              </p>
+              <button onClick={this.onClickGenerateRandom}>Generate random number</button>
+              <p>{this.state.randomNumber}</p>
+            </div>
+            :
+            <div>
+              The random number generator is turned off. Go to your launch darkly dashboard to turn it on.
+            </div>
+        }
+      </div>
+    );
+  }
+}
+
+export default withFlags(Home);
